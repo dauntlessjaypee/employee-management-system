72c7e2cd282df4fc3b5a4568ebc852fd7a2ebb9a
Jay Pee <jaypeetech@outlook.com>
2026-09-01 14:51:22 +0000

    Fix CORS for Kubernetes frontend

--- backend/employee-management-backend/src/main/java/com/employeemanagement/backend/config/CorsConfig.java
+++ backend/employee-management-backend/src/main/java/com/employeemanagement/backend/config/CorsConfig.java
@@ -10,7 +10,11 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173", "http://127.0.0.1:5173")
+        .allowedOrigins(
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "http://192.168.232.211:31941"
+)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
 }
